d3882886a61703006761d61b37a86e3ba2e9bd0b
Maels1991 <r.wan.guillou+github@gmail.com>
2018-11-26 22:14:37 +0100

    Add some dummy data

---
+++ src/Data/Conversations.js
@@ -0,0 +1,23 @@
+import Conversation from '../Models/Conversation';
+import Participant from '../Models/Participant';
+
+const conversation1 = new Conversation();
+conversation1.addParticipant(new Participant('Florence'));
+
+const conversation2 = new Conversation();
+conversation1.addParticipant(new Participant('Edwin'));
+
+const conversation3 = new Conversation();
+conversation1.addParticipant(new Participant('Claude'));
+
+const conversation4 = new Conversation();
+conversation1.addParticipant(new Participant('Elisabeth'));
+
+const CONVERSATIONS = [
+  conversation1,
+  conversation2,
+  conversation3,
+  conversation4
+];
+
+export default CONVERSATIONS;
